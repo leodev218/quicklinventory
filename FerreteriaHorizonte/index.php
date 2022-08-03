1c40c0c1297d30efc87d05aafd48d7bffcde783e
leodev218 <leonardo.dev218@gmail.com>
2022-08-03 00:16:06 -0500

    modificacion estilos

--- FerreteriaHorizonte/index.php
+++ FerreteriaHorizonte/index.php
@@ -5,92 +5,156 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style/style.css"> 
     <!-- Bootstrap CSS v5.2.0-beta1 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
   </head>
   <body>
   <!-- BARRA DE NAVEGACION-->    
-  <nav class="navbar">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
     <a class="navbar-brand" href="#">
-      <img src="img/Saman.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
-    FastFile
-    </a>
-  </div>
-  <div class="container-btn">
-  <button type="button" class="btn btn-secondary btn-lg">Iniciar sesion</button>
+              <img src="img/Saman.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            FastFile
+            </a> 
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+
+        <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+               Opciones
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">crear producto</a></li>
+                <li><a class="dropdown-item" href="#">ver producto</a></li>
+                <li><a class="dropdown-item" href="#">modificar producto</a></li>
+                <li><a class="dropdown-item" href="#">borrar producto</a></li>
+              </ul>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
+        <button class="btn btn-outline-success" type="submit">Inicio</button>
+      </form>
+    </div>
   </div>
 </nav>
- <!-- ETIQUETAS-->  
-<ul class="nav justify-content-center">
-    <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Producto
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-    </li>
-    <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Inventario
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-    </li>
-    <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Ventas
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-    </li>
-    <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Clientes
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-    </li>
- 
-</ul>
- <!-- TARJETAS-->  
-<div class="card mb-3">
-  <img src="img/logo_horizonte.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-</div>
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-  <img src="..." class="card-img-bottom" alt="...">
-</div>
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+    <!-- ETIQUETAS-->  
+    <ul class="nav justify-content-center">
+        <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Producto
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">crear producto</a></li>
+                <li><a class="dropdown-item" href="#">ver producto</a></li>
+                <li><a class="dropdown-item" href="#">modificar producto</a></li>
+                <li><a class="dropdown-item" href="#">borrar producto</a></li>
+              </ul>
+        </li>
+        <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Inventario
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">crear orden</a></li>
+                <li><a class="dropdown-item" href="#">ver orden</a></li>
+                <li><a class="dropdown-item" href="#">modificar orden</a></li>
+                <li><a class="dropdown-item" href="#">borrar orden</a></li>
+              </ul>
+        </li>
+        <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Ventas
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">crear factura</a></li>
+                <li><a class="dropdown-item" href="#">ver factura</a></li>
+                <li><a class="dropdown-item" href="#">modificar producto</a></li>
+                <li><a class="dropdown-item" href="#">borrar producto</a></li>
+              </ul>
+        </li>
+        <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Clientes
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">crear producto</a></li>
+                <li><a class="dropdown-item" href="#">ver producto</a></li>
+                <li><a class="dropdown-item" href="#">modificar producto</a></li>
+                <li><a class="dropdown-item" href="#">borrar producto</a></li>
+              </ul>
+        </li>
+    
+    </ul>
+    <!-- TARJETAS-->  
+    <div class="card mb-3">
+      <img src="img/logo_horizonte.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus adipisci dolorem, ut voluptatum molestias accusamus possimus blanditiis distinctio neque mollitia, in incidunt consectetur tenetur. Necessitatibus pariatur quos sunt veritatis accusamus.
+        Sunt aperiam commodi dolor. Illo quam, quos impedit corrupti voluptas deleniti vitae quod est dolore esse eligendi nostrum illum quaerat distinctio sequi voluptatem molestiae fugiat. Dicta unde sed sapiente dignissimos!
+        Nihil magnam expedita assumenda at repudiandae soluta necessitatibus sapiente voluptatum similique ducimus quis autem consequatur mollitia, laborum modi consectetur fugiat quae deleniti officia reprehenderit voluptates quas! Quae velit mollitia laborum.
+        Adipisci, iure debitis earum, ipsam dicta eligendi minus impedit blanditiis sunt est magnam exercitationem pariatur delectus? Tempore at quibusdam rem facere. Quia earum pariatur explicabo fuga quas molestiae iste repellat!
+        Quos modi autem repellat est, velit temporibus ea quae fugiat. Repellat assumenda quasi hic officiis, enim quas saepe est sequi magni ullam, minima tempore quos non quis esse? Sunt, perferendis!</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+
+    <!-- FOOTER-->
+
+  <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+          
+              <div class="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facere reiciendis commodi soluta quis porro ut officia vel quibusdam! Nesciunt facilis ab autem impedit, voluptate eos nulla voluptatum fugiat! Expedita?
+                  <p>Some representative placeholder content for the first slide.</p>
+              </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="2000">
+                
+            <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit magni debitis asperiores laboriosam distinctio, facere, optio obcaecati omnis possimus eaque aliquid totam iste maxime provident sunt modi nemo tempore voluptatem.
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+            <div class="carousel-item">
+               
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Third slide label</h5>
+             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci natus aspernatur unde sed placeat ullam laboriosam doloremque minus animi, similique assumenda praesentium laborum sit impedit quos at nemo quia voluptates!
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+          </button>
+      </div>
+  
+        <!-- Bootstrap JavaScript Libraries -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
   </body>
 </html>
