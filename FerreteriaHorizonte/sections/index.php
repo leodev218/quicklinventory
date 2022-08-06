496c3006659b5af976c5a0aef02eed182431b7ba
leodev218 <leonardo.dev218@gmail.com>
2022-08-05 21:48:06 -0500

    sections añadida

---
+++ FerreteriaHorizonte/sections/index.php
@@ -0,0 +1,11 @@
+<!-- CABECERA/ HEADER DE LA APP-->
+<?php include ("../templates/header.php")?>
+
+<!-- CUERPO/ BODY DE LA APP-->
+
+
+
+<!-- PIE/ FOOTER DE LA APP-->
+<?php include ("../templates/footer.php")?>
+
+
