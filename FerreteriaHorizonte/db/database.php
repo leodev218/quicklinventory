7f7aeaeb9ce359dfb4c36c0559d3a0b6fcdd6089
leodev218 <leonardo.dev218@gmail.com>
2022-08-10 22:30:17 -0500

    conexion pdo

--- FerreteriaHorizonte/db/database.php
+++ FerreteriaHorizonte/db/database.php
@@ -1,22 +1,26 @@
 <?php
+
+class Conectarse {
 // VALORES DE CONEXION A BASES DE DATOS
- $hostDB= "localhost";
- $userDB = "root";
- $passwordDB = "";
- $selesctDB = "fastfile";
- // FUNCTION PARA REALIZAR LA CONEXION A LA BASE DE DATOS
-function conexion()
-{
-    $conexionBD = new PDO ($hostDB,$userDB,$passwordDB,$selesctDB);
-    if ($conexionBD -> connect_errno);
+   var  $hostDB= "localhost",
+        $userDB = "root",
+        $passwordDB = "",
+        $selesctDB = "fastfile";
+// FUNCTION PARA REALIZAR LA CONEXION A LA BASE DE DATOS
+    function conexion()
     {
-        echo "error de conexion mysql (".$conexionBD -> connect_errno.") ". $conexionBD -> connect_error;
+         $conexionBD = new PDO ($this->$hostDB,$this->$userDB,$this->$passwordDB,$this->$selesctDB);
+       if ($conexionBD -> connect_errno);
+       {
+           echo "error de conexion mysql (".$conexionBD -> connect_errno.") ". $conexionBD -> connect_error;
+       }
+               else {
+                   echo $conexionBD -> host_info."\n <strong>conexion establecida </strong>";
+               } 
+           
     }
-            else 
-            {
-                echo $conexionBD -> host_info."\n <strong>conexion establecida </strong>";
-            }
-}
+
+    }
 
     
 ?>
