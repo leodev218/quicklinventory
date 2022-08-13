c2ec1519f8029854ae7ebd5ca587cb15df561b79
leodev218 <leonardo.dev218@gmail.com>
2022-08-12 23:18:34 -0500

    12 agosto

--- FerreteriaHorizonte/db/database.php
+++ FerreteriaHorizonte/db/database.php
@@ -1,3 +1,12 @@
 <?php
-echo "Hello World!";
+//ARCHIVO QUE CONTIENE LOS DATOS DE LA CONEXION A LA BASE DE DATOS
+
+function connect () 
+{
+    $conn = new PDO ('mysql:host=localhost;dbname=fastfile','root','');
+      return $conn; 
+
+}
+
+
 ?>
