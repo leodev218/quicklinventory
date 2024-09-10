feb18ba0acdbb6e6e7859742c60b947b089a9c7c
leodev218 <leonardo.dev218@gmail.com>
2024-09-10 02:39:06 -0500

    update 10-09

--- FerreteriaHorizonte/sections/cliente.php
+++
@@ -1,9 +0,0 @@
-<!-- CABECERA/ HEADER DE LA APP-->
-<?php include ("../templates/header.php")?>
-
-<!-- CUERPO/ BODY DE LA APP-->
-
-CLIENTE
-
-<!-- PIE/ FOOTER DE LA APP-->
-<?php include ("../templates/footer.php")?>
