d10a7dc42608d151e3203cc34c3cf136b472da0c
leodev218 <leonardo.dev218@gmail.com>
2024-11-22 03:22:49 -0500

    definicion de rutas y navegacion

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -1,46 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from "axios"
+import {Container} from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navigation from './layouts/Navigation'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Info from './pages/Info'
+import Perfil from './pages/Perfil'
+import './css/App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const getEmployees = async () => {
-    try {
-      const url = "http://127.0.0.1:8000/api/v1/employees/";
-      const response = await axios.get(url);
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={getEmployees}>Get elements</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <Router>
+    <Navigation/>
+      <Container fluid>
+        <Routes>
+          <Route path='/' element = {<Home/>}/>
+          <Route path='/products' element = {<Products/>}/>
+          <Route path='/info' element = {<Info/>}/>
+          <Route path='/perfil' element = {<Perfil/>}/>
+        </Routes>
+      </Container>
+  </Router>    
   )
 }
 
---
+++ store/src/pages/Perfil.jsx
@@ -0,0 +1,4 @@
+function Perfil () {
+    return (<h1>Perfil</h1>)
+}
+export default Perfil
