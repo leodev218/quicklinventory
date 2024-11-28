27be916e5c846cf5832414d94a569c7a8d34b62b
leodev218 <leonardo.dev218@gmail.com>
2024-11-27 20:02:41 -0500

    actualizacion pagina de perfil

--- store/src/layouts/Menu.jsx
+++ store/src/layouts/Menu.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Offcanvas from 'react-bootstrap/Offcanvas'
-import Accordion from 'react-bootstrap/Accordion'
 import { FiTool } from "react-icons/fi"
+import ListGroup from 'react-bootstrap/ListGroup'
+import { HiOutlineIdentification ,HiOutlineUserGroup,HiOutlineUsers,HiOutlineTruck } from "react-icons/hi"
 
 function Menu({ ...props }) {
   const [show, setShow] = useState(false)
@@ -12,31 +13,20 @@ function Menu({ ...props }) {
 
   return (
     <>
-      <Button variant="outline-dark" size='sm' onClick={handleShow} className="m-1">
+      <Button variant="outline-dark" size='sm' onClick={handleShow} className="m-1 rounded-0">
         <FiTool className='mb-1'/>
       </Button>
       <Offcanvas show={show} onHide={handleClose} {...props}>
         <Offcanvas.Header className='bg-dark text-light' closeButton>
           <Offcanvas.Title>Opciones</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
-          <Accordion className='align-button' defaultActiveKey='0'>
-            <Accordion.Item eventKey='1'>
-              <Accordion.Header className='bg-dark'>Configuracion</Accordion.Header>
-              <Accordion.Body className='d-grid gap-2'>
-                <Button variant='outline-dark' size='sm'>Opciones avanzadas</Button>
-                <Button variant='outline-dark' size='sm'>Panel de control</Button>
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey='1'>
-              <Accordion.Header>Servicio tecnico</Accordion.Header>
-              <Accordion.Body className='d-grid gap-2'>
-                <Button variant='outline-dark' size='sm'>Enviar reporte</Button>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
+        <Offcanvas.Body className='bg-light'>
+          <ListGroup className="rounded-0">
+            <ListGroup.Item action variant='light'><HiOutlineIdentification  className='me-2'/>Empleado</ListGroup.Item>
+            <ListGroup.Item action variant='light'><HiOutlineUserGroup className='me-2'/>Roles</ListGroup.Item>
+            <ListGroup.Item action variant='light'><HiOutlineUsers className='me-2'/>Clientes</ListGroup.Item>
+            <ListGroup.Item action variant='light'><HiOutlineTruck className='me-2'/>Proveedores</ListGroup.Item>
+          </ListGroup>
         </Offcanvas.Body>
       </Offcanvas>
     </>
--- store/src/pages/Perfil.jsx
+++ store/src/pages/Perfil.jsx
@@ -1,11 +1,17 @@
 import ViewEmployeeTable from "../components/ViewEmployeeTable"
+import { HiOutlineChartBar } from "react-icons/hi"
 import Menu from "../layouts/Menu"
+import {ListGroup,Button} from "react-bootstrap"
 
 function Perfil () {
 
     return (
         <>
-            <Menu/>
+            <ListGroup className='rounded-0' horizontal="sm">
+                <ListGroup.Item className='border-0'><Menu/></ListGroup.Item>
+                <ListGroup.Item className='border-0'><Button variant="outline-dark" className="rounded-0"><HiOutlineChartBar /></Button></ListGroup.Item>
+            </ListGroup>
+
                 <h1>Perfil</h1>
                 <h1>Lista empleados</h1>
                 <ViewEmployeeTable/>
