084c787fbe62bd02d4b34957d015116f999f2cc1
leodev218 <leonardo.dev218@gmail.com>
2024-12-01 11:31:33 -0500

    update sidebar

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
   <Router>
     <Navigation/>
-      <Container fluid>
+      <Container flex>
         <Routes>
           <Route path='/' element = {<Home/>}/>
           <Route path='/products' element = {<Products/>}/>
--- store/src/layouts/Menu.jsx
+++ store/src/layouts/Menu.jsx
@@ -4,8 +4,10 @@ import Offcanvas from 'react-bootstrap/Offcanvas'
 import { HiChevronDoubleRight } from "react-icons/hi"
 import ListGroup from 'react-bootstrap/ListGroup'
 import { HiOutlineIdentification,HiOutlineUsers,HiOutlineTruck } from "react-icons/hi"
-import { Nav}from 'react-bootstrap'
+import { CardGroup, Nav}from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import logo from '../assets/4.gif'
+import Card from 'react-bootstrap/Card'
 
 function Menu({ ...props }) {
   const [show, setShow] = useState(false)
@@ -19,10 +21,15 @@ function Menu({ ...props }) {
         <HiChevronDoubleRight className='mb-1'/>
       </Button>
       <Offcanvas show={show} onHide={handleClose} {...props}>
-        <Offcanvas.Header className='bg-dark text-light' closeButton>
+        <Offcanvas.Header className='navbar text-light' closeButton>
           <Offcanvas.Title>Opciones</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body className='bg-light'>         
+        <Offcanvas.Body className='bg-light'>  
+            <CardGroup>
+              <Card border='light'>
+                <Card.Img variant='top' src={logo}/>
+              </Card>
+            </CardGroup>    
           <ListGroup className="rounded-0">
             <Nav.Link>
               <ListGroup.Item action onClick={handleClose} as={Link} to="/employee" variant='light'>
